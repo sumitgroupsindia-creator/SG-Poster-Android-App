9c9696e4b53239c5399e814780e2b12f995abdd6
Satish Bohra <satish.bohra@gmail.com>
2026-04-01 07:02:19 +0530

    Update version code to 45 and version name to 2.9 in build.gradle; update APP_VERSION in Config.java to 2.9

--- app/src/main/java/com/iqueen/brandpeak/Config.java
+++ app/src/main/java/com/iqueen/brandpeak/Config.java
@@ -9,7 +9,7 @@ public class Config {
      * AppVersion
      * Current App Version
      */
-    public static String APP_VERSION = "2.8";
+    public static String APP_VERSION = "2.9";
 
     /**
      * API Related
